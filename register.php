64ae56285cbd348c471a255bd74c599ae768689f
Kim Terrence Quines <quineskimterrence@gmail.com>
2024-11-29 06:34:27 +0800

    Smart Switch

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+    echo("Hello World");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Switch</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Firstname</label>
+        <input type="text" name="fname">
+        <label for="">Lastname</label>
+        <input type="text" name="lname">
+        <label for="">Username</label>
+        <input type="text" name="uname">
+        <label for="">Password</label>
+        <input type="text" name="pw">
+        <label for="">Confirm Password</label>
+        <input type="text" name="cpw">
+        <button >Register</button>
+    </form>
+</body>
+</html>
