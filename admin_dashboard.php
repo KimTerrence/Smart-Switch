e36f3d7948f9f9b5b63706486ca17977871b4845
Kim Terrence Quines <quineskimterrence@gmail.com>
2024-12-23 08:22:11 +0800

    changesbin admin_dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,7 +1,6 @@
 <?php 
 session_start();
 include 'database_config.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -9,19 +8,37 @@ include 'database_config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin </title>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css  "/>
+    <script src="./bootstrap/js/bootstrap.js"></script>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
     <p>Admin Dashboard</p>
+    <table class="table table-striped table-hover w-50  "> 
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th >Status</th>
+            <th colspan="3">Edit</th>
+        </tr>
     <?php 
-      $sql = "SELECT * FROM user_info where status = 'new' ";
-      $result = $conn->query($sql);
-
-        $row = $result->fetch_assoc();
-    echo $row['firstname'] . ' ' . $row['lastname'];
-
-        
-    
+        $sql = "SELECT * FROM user_info "; //get data from database
+        $result = $conn->query($sql); // query
+        while ($row = $result->fetch_assoc()) { //display data
     ?>
+    <tr>
+        <td> <?php echo $row['firstname']; ?> </td>
+        <td> <?php echo $row['lastname']; ?> </td>
+        <td> <?php echo $row['status']; ?> </td>
+        <td><img src="./assets/check.svg" alt="verify"></td>
+        <td><img src="./assets/close.svg" alt="decline"></td>
+        <td><img src="./assets/delete_b.svg" alt="delete"></td>
+    </tr>
+
+     <?php   
+    }
+    ?>
+</table>
 </body>
 </html>
