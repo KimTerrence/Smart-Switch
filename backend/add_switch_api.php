527181d414c0d8b399f8394c8806095e9badd52f
Kim Terrence Quines <quineskimterrence@gmail.com>
2025-01-03 12:06:25 +0800

    fix switch

--- backend/add_switch_api.php
+++ backend/add_switch_api.php
@@ -4,8 +4,10 @@
     if($_SERVER["REQUEST_METHOD"] = "POST"){
         $name = $_POST['name'];
         $ip = $_POST['ip'];
+        $light = $_POST['light'];
+        $motion = $_POST['motion'];
 
-        $sql= "INSERT INTO switch (name, ip_address) VALUE ( '$name', '$ip') ";
+        $sql= "INSERT INTO switch (name, ip_address, switch, motion, light) VALUE ( '$name', '$ip' , 'off', '$motion', '$light') ";
 
         if ($conn->query($sql) === TRUE) {
             //  header("Location: login.php");
