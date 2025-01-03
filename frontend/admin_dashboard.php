71ae6b64128e9c419e04387d48f99fa032647fc1
Kim Terrence Quines <quineskimterrence@gmail.com>
2025-01-03 12:06:56 +0800

    admin fix

--- frontend/admin_dashboard.php
+++ frontend/admin_dashboard.php
@@ -57,8 +57,22 @@ include '../backend/database_config.php';
                             </div>
                             <div class="modal-body">
                                 <form action="../backend/add_switch_api.php" method="POST" class="d-flex justify-content-center flex-column p-5 gap-2">
-                                    <input type="text" name="name" placeholder="Name:" class="form-control">
-                                    <input type="text" name="ip" placeholder="Ip Address:" class="form-control">
+                                    <input type="text" name="name" placeholder="Name:" class="form-control" required>
+                                    <input type="text" name="ip" placeholder="Ip Address:" class="form-control" required>
+                                   <div class="d-flex gap-3 align-items-center justify-content-between">
+                                        <label for="motion">Motion Sensor</label>
+                                        <select name="motion" id="motion" class="form-select w-50">
+                                            <option value="disabled">Disable</option>
+                                            <option value="off">Enable</option>
+                                        </select>
+                                   </div>
+                                   <div class="d-flex gap-3 align-items-center justify-content-between">
+                                        <label for="light">Ligth Sensor</label>
+                                        <select name="light" id="light" class="form-select w-50">
+                                            <option value="disabled">Disable</option>
+                                            <option value="off">Enable</option>
+                                        </select>
+                                   </div>
                                     <button type="submit" class="btn btn-primary">Save changes</button>
                                 </form>
                                  <div class="modal-footer">
@@ -72,8 +86,11 @@ include '../backend/database_config.php';
 
             <table class="table table-striped table-hover w-100">
                 <tr>
-                    <th>Switch</th>
+                    <th>Name</th>
                     <th>IP</th>
+                    <th>Switch</th>
+                    <th>Motion</th>
+                    <th>Light</th>
                     <th>Edit</th>   
                 </tr>
 
@@ -86,6 +103,9 @@ include '../backend/database_config.php';
                 <tr>
                     <td> <?php echo $switch['name']; ?> </td>
                     <td> <?php echo $switch['ip_address']; ?> </td>
+                    <td> <?php echo $switch['switch']; ?> </td>
+                    <td> <?php echo $switch['motion']; ?> </td>
+                    <td> <?php echo $switch['light']; ?> </td>
                     <td><img src="./assets/delete_b.svg" alt="delete"></td>
                 </tr>
                 
